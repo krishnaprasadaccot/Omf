de7b995b32105553eb41ef10279d2b8a07e428a0
krishnaprasadaccot@gmail.com <krishnaprasadaccot@gmail.com>
2019-09-11 09:05:14 -0700

    Search,Review,Customer,Order Services

---
+++ src/Omf.Services.Review/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Omf.Common.Events;
+using Omf.Common.Services;
+
+namespace Omf.Services.Review
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ServiceHost.Create<Startup>(args)
+                .UseRabbitMq()
+                .SubscribeToEvent<ActivityCreated>()
+                .Build()
+                .Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
